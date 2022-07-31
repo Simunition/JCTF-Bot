4b9315b95bcb655e533a3b35df91095ebc06f4a0
Ubuntu <ubuntu@ip-10-0-17-71.ec2.internal>
2022-07-31 15:14:09 +0000

    updates

--- register.js
+++ register.js
@@ -7,9 +7,22 @@ const token = process.env.BOT_TOKEN;
 const clientId = process.env.APP_ID;
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('hello').setDescription('Replies with hello!'),
-	new SlashCommandBuilder().setName('givemetheflag').setDescription('??')
+	new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with pong!'),
+	new SlashCommandBuilder()
+		.setName('hello')
+		.setDescription('Replies with hello!'),
+	new SlashCommandBuilder()
+		.setName('givemetheflag')
+		.setDescription('??'),
+	new SlashCommandBuilder()
+		.setName('start_instance')
+		.setDescription('Start given instance')
+		.addStringOption(option =>
+			option.setName('instance')
+			.setDescription('The instance to start')
+			.setRequired(true))
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
